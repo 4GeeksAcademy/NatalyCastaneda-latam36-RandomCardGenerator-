9e183a22fe6364bfa94afc236d168a7d84d87f6d
Nath <115433779+Nath1710@users.noreply.github.com>
2024-10-02 02:52:38 +0000

    add javascript logic

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //   //write your code here
+  let sticks = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+  let values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let randomStick = sticks[Math.floor(Math.random() * sticks.length)];
+  let randomValue = values[Math.floor(Math.random() * values.length)];
+  document.querySelector("#topStick").innerHTML = randomStick;
+  document.querySelector("#numbersLiterals").innerHTML = randomValue;
+  document.querySelector("#bottomStick").innerHTML = randomStick;
+
+  if (randomStick === "&hearts;" || randomStick === "&diams;") {
+    document.querySelector("#topStick").style.color = "red";
+    document.querySelector("#numbersLiterals").style.color = "red";
+    document.querySelector("#bottomStick").style.color = "red";
+  }
 };
