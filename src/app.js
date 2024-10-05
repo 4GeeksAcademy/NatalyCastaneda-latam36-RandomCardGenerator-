db6193ee9312a5eceff4b916651eee2d8d36dd93
Nath <115433779+Nath1710@users.noreply.github.com>
2024-10-05 01:59:43 +0000

    add function newcard, with setinterval and button for manual trigger

--- src/app.js
+++ src/app.js
@@ -23,16 +23,45 @@ window.onload = function() {
     "Q",
     "K"
   ];
+  function newCard() {
+    let randomStick = sticks[Math.floor(Math.random() * sticks.length)];
+    let randomValue = values[Math.floor(Math.random() * values.length)];
+    document.querySelector("#topStick").innerHTML = randomStick;
+    document.querySelector("#numbersLiterals").innerHTML = randomValue;
+    document.querySelector("#bottomStick").innerHTML = randomStick;
 
-  let randomStick = sticks[Math.floor(Math.random() * sticks.length)];
-  let randomValue = values[Math.floor(Math.random() * values.length)];
-  document.querySelector("#topStick").innerHTML = randomStick;
-  document.querySelector("#numbersLiterals").innerHTML = randomValue;
-  document.querySelector("#bottomStick").innerHTML = randomStick;
+    if (randomStick === "&hearts;" || randomStick === "&diams;") {
+      document.querySelector("#topStick").style.color = "red";
+      document.querySelector("#numbersLiterals").style.color = "red";
+      document.querySelector("#bottomStick").style.color = "red";
+    } else {
+      document.querySelector("#topStick").style.color = "black";
+      document.querySelector("#numbersLiterals").style.color = "black";
+      document.querySelector("#bottomStick").style.color = "black";
+    }
+  }
+  document.querySelector("#clickMe").addEventListener("click", newCard);
 
-  if (randomStick === "&hearts;" || randomStick === "&diams;") {
-    document.querySelector("#topStick").style.color = "red";
-    document.querySelector("#numbersLiterals").style.color = "red";
-    document.querySelector("#bottomStick").style.color = "red";
+  newCard();
+
+  setInterval(newCard, 10000);
+
+  function resizeCard() {
+    document.getElementById("card").style.width =
+      document.getElementById("WidthInput").value + "px";
+    document.getElementById("card").style.height =
+      document.getElementById("HeightInput").value + "px";
+  }
+  document
+    .querySelector("#resizeCardBtn")
+    .addEventListener("click", resizeCard);
+  function adjustFontSize(container, desiredWidth) {
+    var fontSize = parseInt(
+      window.getComputedStyle(container, null).getPropertyValue("font-size")
+    );
+    while (container.scrollWidth > desiredWidth && fontSize > 0) {
+      fontSize--;
+      container.style.fontSize = fontSize + "px";
+    }
   }
 };
